b8815a75b1e99c3a9ac05f37ed5a5c36d76cc0bf
nordin001 <73000909+nordin001@users.noreply.github.com>
2023-11-18 16:27:49 +0100

    js tests
    El etorno de desarollar tests en javascript
    :no escompleto ,falta añadir los mocks , para testear el proceso de llamar ficheros php sin llamar al php

--- app/js/server.js
+++ app/js/server.js
@@ -1,6 +1,6 @@
 
 //-----------------------------------------------------------------------------------------------------------
-//              recuperarprueba()
+//              recuperarprueba()!!
 //-----------------------------------------------------------------------------------------------------------
 
 function recuperarprueba() {
@@ -195,7 +195,7 @@ function registro() {
 }
 
 //-------------------------------------------------------------------------------------------------------
-//         obtenerUserId()
+//         obtenerUserId()!!
 //-------------------------------------------------------------------------------------------------------
 async function obtenerUserId() {
     const response = await fetch('../api/userid.php'); // Ruta al script PHP que obtiene $_SESSION['user_id']
@@ -218,7 +218,7 @@ async function obtenerUserId() {
 }
 
 //-------------------------------------------------------------------------------------------------------
-//         hola()
+//         hola()!!
 //-------------------------------------------------------------------------------------------------------
 async function hola()
 {
@@ -320,6 +320,70 @@ async function comprobarsesion()
         }   
     });
 }
+//---------------------------------------------------------------------------------------------------------------------
+//       Tallal:we have to separate functions to smallee portions , in order to test them easly 
+//---------------------------------------------------------------------------------------------------------------------
+
+//Dom interaction
+function getFormData() {
+    return {
+      email: document.getElementById('email').value,
+      nombre: document.getElementById('nombre').value,
+      contrasenya: document.getElementById('contrasenya').value,
+      telefono: document.getElementById('telefono').value,
+      confirmarcontrasenya: document.getElementById('confirmarcontrasenya').value
+    };
+  }
+
+//validation function
+function validateForm(formData) {
+    if (formData.email === "" || formData.nombre === "" || formData.contrasenya === "" || formData.telefono === "" || formData.confirmarcontrasenya === "") {
+      alert("Por favor, complete todos los campos.");
+      return false;
+    }
+  
+    if (formData.contrasenya !== formData.confirmarcontrasenya) {
+      alert("La confirmación de la contraseña no coincide.");
+      return false;
+    }
+  
+    return true;
+  }
+
+  //Api call function
+  async function makeApiCall(formData) {
+    try {
+      const response = await $.ajax({
+        url: '../api/registro.php',
+        type: 'POST',
+        data: { email: formData.email, nombreyapellidos: formData.nombre, contrasenya: formData.contrasenya, telefono: formData.telefono },
+        dataType: 'json'
+      });
+  
+      alert("El usuario se creo correctamente");
+      window.location.href = "login.html";
+    } catch (error) {
+      console.error('Error al obtener el valor.', error);
+    }
+  }
+  //registro2
+  function registro2() {
+    $(document).ready(function () {
+      const formData = getFormData();
+  
+      if (!validateForm(formData)) {
+        return;
+      }
+  
+      makeApiCall(formData);
+    });
+  }
+  
+  
+
+  
+  
+
 
 //---------------------------------------------------------------------------------------------------------------------
 //       a simple function for testing the tests :)
@@ -332,4 +396,10 @@ const addNumbers = (a, b) => {
   //es importante meter las funciones que vamos a usar aqui
   module.exports = {
     addNumbers,
+    registro,
+    getFormData,
+    validateForm,
+    makeApiCall,
+    registro2
+
   };
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,13 @@
 // test.js
 const chai = require('chai');
 const { expect } = chai;
+const sinon = require('sinon');
 
 // Import the function to test from yourCode.js
 const { addNumbers } = require('../app/js/server');
+const { registro } = require('../app/js/server');
+const registeroPHP = require('../src/registro.php');
+
 
 describe('addNumbers function', () => {
   it('should correctly add two numbers', () => {
@@ -12,4 +16,23 @@ describe('addNumbers function', () => {
   });
 
   // Add more test cases as needed
+});
+
+
+
+describe('registerUser function', () => {
+  it('should register a new user', async () => {
+    // Create a sinon stub to mock the PHP file or HTTP request
+    const phpFileStub = sinon.stub(registeroPHP, 'yourPhpFunction').resolves('success');
+
+    // Call registerUser
+    const result = await registerUser(/* parameters */);
+
+    // Assert the result and ensure the PHP file or HTTP request was called
+    expect(result).to.equal('success');
+    sinon.assert.calledOnce(phpFileStub);
+
+    // Restore the original function after the test
+    phpFileStub.restore();
+  });
 });
